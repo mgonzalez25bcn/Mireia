b891de2ae1d522cb9ed083f7579c98c6ec3d2d6c
Mireia <mgonzalez25@politecnics.barcelona>
2026-07-14 16:11:26 +0200

    objeto bares

---
+++ 20260714_pasaporte_bares/bares.js
@@ -0,0 +1,98 @@
+const bares = [
+  {
+      id: 1,
+      nombre: "Bar Atlanta",
+      visitado: false
+  },
+  {
+      id: 2,
+      nombre: "Bar El Toro",
+      visitado: false
+  },
+  {
+      id: 3,
+      nombre: "Fat Cat",
+      visitado: false
+  },
+  {
+      id: 4,
+      nombre: La Prudència",
+      visitado: false
+  },
+  {
+      id: 5,
+      nombre: "Las Fernández",
+      visitado: false
+  },
+  {
+      id: 6,
+      nombre: "Nun Cultural Café",
+      visitado: false
+  },
+  {
+      id: 7,
+      nombre: "Chula Vista",
+      visitado: false
+  },
+  {
+      id: 8,
+      nombre: "El Magraner Boig",
+      visitado: false
+  },
+  {
+      id: 9,
+      nombre: "El Cafetí",
+      visitado: false
+  },
+  {
+      id: 10,
+      nombre: "La Ravala",
+      visitado: false
+  },
+  {
+      id: 11,
+      nombre: "La Monroe",
+      visitado: false
+  },
+  {
+      id: 12,
+      nombre: "Arume",
+      visitado: false
+  },
+  {
+      id: 13,
+      nombre: "La Morera",
+      visitado: false
+  },
+  {
+      id: 14,
+      nombre: "Bar Mendizábal",
+      visitado: false
+  },
+  {
+      id: 15,
+      nombre: "Dr. Beer & Mr. Fried",
+      visitado: false
+  },
+  {
+      id: 16,
+      nombre: "Biocenter",
+      visitado: false
+  },
+  {
+      id: 17,
+      nombre: "Bar Costa",
+      visitado: false
+  },
+  {
+      id: 18,
+      nombre: "Clemen's Boqueria",
+      visitado: false
+  },
+  {
+      id: 19,
+      nombre: "L'Universal",
+      visitado: false
+  },
+
+]
