c86146620522d482ab2eec01c3372e351562bd4f
Mireia <mgonzalez25@politecnics.barcelona>
2026-07-17 18:57:16 +0200

    nuevos planes

--- 20260716_planes/planes.js
+++ 20260716_planes/planes.js
@@ -43,33 +43,76 @@ let planes = [
 
 const listaPlanes = document.getElementById('listaPlanes')
 
-for (let plan of planes){
-  // console.log(plan.nombre)
-  let mensajeEstado
-  let claseEstado
+  function mostrarPlanes(){
+    listaPlanes.innerHTML = ''
+    for (let i=0; i < planes.length; i++){
+    // for (let plan of planes){
+      // console.log(plan.nombre)
+      let plan = planes[i] // necesito esta variable desde que cambio la definición del bucle
+      let mensajeEstado
+      let claseEstado
 
-  if(plan.reservado === true){
-    mensajeEstado = 'Ya está reservado'
-    claseEstado = 'reservado'
-  } else {
-    mensajeEstado = 'Plan disponible'
-    claseEstado = 'disponible'
+      if(plan.reservado === true){
+        mensajeEstado = 'Ya está reservado'
+        claseEstado = 'reservado'
+      } else {
+        mensajeEstado = 'Plan disponible'
+        claseEstado = 'disponible'
+      }
+
+      // Poner gratis si precio es cero
+      let textoPrecio
+      if (plan.precio === 0){
+          textoPrecio = 'Gratis'
+      } else {
+        textoPrecio = `${plan.precio} €`
+      }
+ 
+      listaPlanes.innerHTML += `
+      <article class='tarjeta'>
+      <div class='contenedorImagen'>
+        <img src=${plan.imagen} alt='${plan.nombre}'>
+        <span class='icono'>${plan.icono}</span>
+      </div>
+      <div class='contenidoTarjeta'>
+       <p class='categoria'>${plan.categoria}</p>
+        <h2>${plan.icono} ${plan.nombre}</h2>
+        <p>Lugar: ${plan.lugar}</p>
+        <p>Precio: ${textoPrecio}</p>
+        <p class='estado ${claseEstado}'>${mensajeEstado}</p>
+        <button class='btnReservar' data-indice='${i}'>Cambiar reserva</button>
+      </div>
+    </article>
+      `
   }
 
-  listaPlanes.innerHTML += `
-  <article class='tarjeta'>
-  <div class='contenedorImagen'>
-    <img src=${plan.imagen} alt='${plan.nombre}'>
-    <span class='icono'>${plan.icono}</span>
-  </div>
-  <div class='contenidoTarjeta'>
-    <p class='categoria'>${plan.categoria}</p>
-    <h2>${plan.icono} ${plan.nombre}</h2>
-    <p>Lugar: ${plan.lugar}</p>
-    <p>Precio: ${plan.precio}</p>
-    <p>${mensajeEstado}</p>
-  </div>
-  </article>
-  `
+  // contiene todos los botones reservar pq lo declaramos All
+  let botonesReservar = document.querySelectorAll('.btnReservar')
+
+  //nos llevará a cada botón (busca el índice de cada botón)
+  for(let boton of botonesReservar){
+    boton.addEventListener('click', cambiarReserva)
 }
+}
+
+function cambiarReserva(ev){
+      let indice = ev.target.dataset.indice
+      // console.log(indice)
+      planes[indice].reservado = !planes[indice].reservado
+
+      mostrarPlanes()
+}
+
+planes.push({
+  nombre: 'Beer Pong',
+  lugar: 'Playa',
+  precio: 'lo que te valga la cerveza',
+  categoria: 'Deporte',
+  icono: '🍺',
+  imagen: 'img/beerpong.jpg',
+  reservado: false
+}
+)
+
+mostrarPlanes()
 
